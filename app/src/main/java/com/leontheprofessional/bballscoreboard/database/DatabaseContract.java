3a6eaaf3a32874ebc30824cd5b0c57fd86b1989c
Leon <yanglyu.leon.7@gmail.com>
2016-05-26 15:30:59 -0700

    DatabaseHelper setup
    Based on the very simple yet inefficient DBDesign

--- app/src/main/java/com/leontheprofessional/bballscoreboard/database/DatabaseContract.java
+++ app/src/main/java/com/leontheprofessional/bballscoreboard/database/DatabaseContract.java
@@ -6,15 +6,35 @@ public final class DatabaseContract{
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "scoreboard.db";
+    public static final String COMMA_SEPARATOR = ", ";
+    public static final String TYPE_INTEGER = " INTEGER";
+    public static final String TYPE_DATETIME = " DATETIME";
+
+    public static final String TABLE_CREATION = "CREATE TABLE " +
+            PerformanceTable.TABLE_NAME + " (" +
+            PerformanceTable.COLUMN_JERSEY_NUMBER + " INTEGER" + COMMA_SEPARATOR +
+            PerformanceTable.COLUMN_PT3 + TYPE_INTEGER + COMMA_SEPARATOR +
+            PerformanceTable.COLUMN_PT_2 + TYPE_INTEGER + COMMA_SEPARATOR +
+            PerformanceTable.COLUMN_PT_1 + TYPE_INTEGER + COMMA_SEPARATOR +
+            PerformanceTable.COLUMN_STEAL + TYPE_INTEGER + COMMA_SEPARATOR +
+            PerformanceTable.COLUMN_O_REB + TYPE_INTEGER + COMMA_SEPARATOR +
+            PerformanceTable.COLUMN_D_REB + TYPE_INTEGER + COMMA_SEPARATOR +
+            PerformanceTable.COLUMN_FAUL + TYPE_INTEGER + COMMA_SEPARATOR +
+            PerformanceTable.COLUMN_TURNOVER + TYPE_INTEGER + COMMA_SEPARATOR +
+            PerformanceTable.COLUMN_BLOCK + TYPE_INTEGER + COMMA_SEPARATOR +
+            PerformanceTable.COLUMN_TIMESTAMP + TYPE_DATETIME + COMMA_SEPARATOR + ")";
+
+    public static final String TABLE_DELETION = "DROP TABLE IF EXISTS " + PerformanceTable.TABLE_NAME;
+
 
     // To prevent someone from accidentally instantiating the contract class
     // have an empty constructor
     private DatabaseContract(){}
 
     // todo: This is the very simple table, very inefficient. Improvement will be made later on
-    public static abstract class PerformanceContract implements BaseColumns {
+    public static abstract class PerformanceTable implements BaseColumns {
         public static final String TABLE_NAME = "performance_table";
-        public static final String JERSEY_NUMBER = "jersey_number";
+        public static final String COLUMN_JERSEY_NUMBER = "jersey_number";
         public static final String COLUMN_PT3 = "pt_3";
         public static final String COLUMN_PT_2 = "pt_2";
         public static final String COLUMN_PT_1 = "pt_1";
@@ -24,60 +44,59 @@ public final class DatabaseContract{
         public static final String COLUMN_FAUL = "faul";
         public static final String COLUMN_TURNOVER = "turnover";
         public static final String COLUMN_BLOCK = "block";
-        public static final String COLUMN_TIMESTAMP = "timestamp";
-        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_TIMESTAMP = "datetimestamp";
     }
 
-    public static abstract class Pt3Contract implements BaseColumns{
+    public static abstract class Pt3Table implements BaseColumns{
         public static final String TABLE_NAME = "pt_3_table";
         public static final String COLUMN_TIMESTAMP = "timestamp";
         public static final String Column_PT3_TOTAL = "pt_3_total";
         public static final String COLUMN_DATE = "date";
     }
 
-    public static abstract class Pt2Contract implements BaseColumns{
+    public static abstract class Pt2Table implements BaseColumns{
         public static final String TABLE_NAME = "pt_2_table";
         public static final String COLUMN_TIMESTAMP = "timestamp";
         public static final String COLUMN_PT2_TOTAL = "pt_2_total";
         public static final String COLUMN_DATE = "date";
     }
 
-    public static abstract class Pt1Contract implements BaseColumns{
+    public static abstract class Pt1Table implements BaseColumns{
         public static final String TABLE_NAME = "pt_1_table";
         public static final String COLUMN_TIMESTAMP = "timestamp";
         public static final String COLUMN_PT3_TOTAL = "pt_1_total";
         public static final String COLUMN_DATE = "date";
     }
 
-    public static abstract class StealContract implements BaseColumns{
+    public static abstract class StealTable implements BaseColumns{
         public static final String TABLE_NAME = "steal_table";
         public static final String COLUMN_TIMESTAMP = "timestamp";
         public static final String COLUMN_STEAL_TOTAL = "steal_total";
         public static final String COLUMN_DATE = "date";
     }
 
-    public static abstract class ORebContract implements BaseColumns{
+    public static abstract class ORebTable implements BaseColumns{
         public static final String TABLE_NAME = "o_reb_table";
         public static final String COLUMN_TIMESTAMP = "timestamp";
         public static final String COLUMN_O_REB_TOTAL = "o_reb_total";
         public static final String COLUMN_DATE = "date";
     }
 
-    public static abstract class DRebContract implements BaseColumns{
+    public static abstract class DRebTable implements BaseColumns{
         public static final String TABLE_NAME = "d_reb_table";
         public static final String COLUMN_TIMESTAMP = "timestamp";
         public static final String COLUMN_D_REB_TOTAL = "d_reb_total";
         public static final String COLUMN_DATE = "date";
     }
 
-    public static abstract class TurnoverContract implements BaseColumns{
+    public static abstract class TurnoverTable implements BaseColumns{
         public static final String TABLE_NAME = "turnover_table";
         public static final String COLUMN_TIMESTAMP = "timestamp";
         public static final String COLUMN_TURNOVER = "turnover_total";
         public static final String COLUMN_DATE = "date";
     }
 
-    public static abstract class BlockContract implements BaseColumns{
+    public static abstract class BlockTable implements BaseColumns{
         public static final String TABLE_NAME = "block_table";
         public static final String COLUMN_TIMESTAMP = "timestamp";
         public static final String COLUMN_BLOCK_TOTAL = "block_total";
--- app/src/main/java/com/leontheprofessional/bballscoreboard/database/helper/DatabaseHelper.java
+++ app/src/main/java/com/leontheprofessional/bballscoreboard/database/helper/DatabaseHelper.java
@@ -3,20 +3,27 @@ package com.leontheprofessional.bballscoreboard.database.helper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.leontheprofessional.bballscoreboard.database.DatabaseContract;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
 
+    private static final String LOG_TAG = DatabaseHelper.class.getSimpleName();
+
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DatabaseContract.DATABASE_NAME, null, DatabaseContract.DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        Log.v(LOG_TAG, "Table creation command: " + DatabaseContract.TABLE_CREATION);
+        db.execSQL(DatabaseContract.TABLE_CREATION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(DatabaseContract.TABLE_DELETION);
+        onCreate(db);
     }
 }
