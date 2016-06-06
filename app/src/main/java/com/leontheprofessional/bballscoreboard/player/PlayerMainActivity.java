f01c16906b08fd94d971e8ea1cba364c5bef1b52
Leon <yanglyu.leon.7@gmail.com>
2016-06-05 21:00:44 -0700

    Minor changes on TeamMainActivity layout
    Now it's time to get PlayMainActivity work.

--- app/src/main/java/com/leontheprofessional/bballscoreboard/player/PlayerMainActivity.java
+++ app/src/main/java/com/leontheprofessional/bballscoreboard/player/PlayerMainActivity.java
@@ -2,9 +2,6 @@ package com.leontheprofessional.bballscoreboard.player;
 
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by yangl on 6/5/2016.
- */
 public class PlayerMainActivity extends AppCompatActivity{
 
 
