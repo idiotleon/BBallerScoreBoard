9e63d487e5d71c83400262c7d9e7ad3019aa972f
Leon <yanglyu.leon.7@gmail.com>
2016-09-15 16:45:28 -0700

    AddOnePlayerFragment changed to its corresponding DialogFragment.

--- app/src/main/java/com/leontheprofessional/bballscoreboard/player/PlayerMainActivity.java
+++ app/src/main/java/com/leontheprofessional/bballscoreboard/player/PlayerMainActivity.java
@@ -1,24 +1,12 @@
 package com.leontheprofessional.bballscoreboard.player;
 
-import android.content.ContentValues;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
 
 import com.leontheprofessional.bballscoreboard.R;
-import com.leontheprofessional.bballscoreboard.database.DatabaseContract;
 
 public class PlayerMainActivity extends AppCompatActivity {
 
@@ -37,13 +25,9 @@ public class PlayerMainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // todo: commit AddOnePlayerFragment for new players registration
-                AddOnePlayerFragment addOnePlayerFragment = new AddOnePlayerFragment();
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.add(,addOnePlayerFragment);
-                fragmentTransaction.commit();
-
+                // todo: commit AddOnePlayerDialogFragment for new players registration
+                AddOnePlayerDialogFragment addOnePlayerDialogFragment = new AddOnePlayerDialogFragment();
+                addOnePlayerDialogFragment.show(getSupportFragmentManager(), "add_one_player_dialog_fragment");
             }
         });
     }
